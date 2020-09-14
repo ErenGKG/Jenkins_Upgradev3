5216a5b3852b22cc0a33e908bd9c2c7dc7773a6d
Eren <eren.degerli@agilatek.com>
2020-09-14 14:08:11 +0300

    first commit

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -16,4 +16,8 @@ public class Greeter {
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
+
+  public String greet2(String someone) {
+    return String.format("Hello Guys, What's the story?, %s!", someone);
+  }
 }
